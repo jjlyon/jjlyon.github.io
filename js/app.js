8cf96ee6b72998dd6f8a66fa452dbec9e2d20159
jjlyon <john.jlyonii@gmail.com>
2021-07-11 11:11:52 -0500

    migrated to vue.js

---
+++ js/app.js
@@ -0,0 +1,40 @@
+Vue.component('app-page', {
+  template: `
+  <div>
+    <h1 id="title" class="center" :style="{color: color}">Plane Excellence</h1>
+    <img src="img/unknown.png" alt="Excellent" class="center" @click="handleClick"></img>
+  </div>
+  `,
+  props: {
+    interval: null,
+    color: '#000000'
+  },
+  methods: {
+    handleClick: function() {
+      if(this.interval) {
+        clearInterval(this.interval);
+        this.interval = null
+      } else {
+        this.interval = setInterval(this.changeColor, 400);
+      }
+    },
+    changeColor: function() {
+      const letters = '0123456789ABCDEF';
+      let color = '#';
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      this.color = color;
+    }
+  },
+  mounted : function(){
+      this.handleClick();
+  },
+  beforeDestroy () {
+     clearInterval(this.interval);
+  }
+});
+
+const vm = new Vue({
+  el: '#app'
+});
